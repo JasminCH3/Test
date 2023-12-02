7d827a466ee8a46d77a04c9f900a4ed44c4baa81
JasminCabreraHerrera <jasmincabreraherrera543@gmail.com>
2023-12-02 10:24:29 -0500

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,76 @@
+/*//var num1texto 
+var num1texto = prompt("Ingrese el primer numero: ");
+var num2texto = prompt("Ingrese el segundo numero: ");
+*/
+/*var num1 = parseInt(num1texto);
+var num2 = parseInt(num2texto);
+
+console.log(typeof(num1));
+
+console.log("Numero 1:",num1);
+console.log("Numero 2:",num2);
+
+var suma = num1 + num2;
+console.log("Suma: " ,suma);
+
+var resta = num1 - num2;
+console.log("Esta es la resta->",resta);
+*/
+/*
+//PRACTICANDO OPERACIONES CON STRINGS
+
+var miTexto = "PRACTICANDO OPERACIONES CON STRINGS \n\nPRACTICANDO OPERACIONES CON STRINGS";
+console.log(miTexto);
+alert(miTexto);
+
+var nombre = "jasmin sas";
+var tam = nombre.length;
+/*console.log(tam);*/
+/*console.log(nombre);
+
+console.log(nombre[2]=='S');*/
+
+
+//creen una variable llamada tam y guarden 
+//el tamaño de la variable nombre
+
+/*var txt = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+var len = txt.length;
+console.log(len);
+*/
+//si la luz del semaforo esta en verde ´puedes cruzar pero si esta rojo no puedes
+var color ="verde"; 
+if(color == "verde"){
+    console.log("Puedes cruzar la psita");
+}
+else{ 
+    console,log("No puedes cruzar.Espera");
+}
+
+// Si soy mayor de 14 años, entonces puedo ver 
+//la pelicula El Aro. Sino, entonces me quedo 
+//afuera.
+var pelicula ="mayor";
+if(pelicula =="mayor"){
+    console.log("si eres mayor a 14, puedes");
+}
+else{
+    console.log("si eres menor de 14, No puedes");
+    
+}
+
+
+
+/*
+Si termino todas mis tareas, entonces puedo salir a jugar.Sino,
+entonces me tengo que quedar a limpiar mi habitacion.
+//Cual va a ser mi variable
+//Armar mi condicion
+//Colocar la accion V y F 
+*/
+
+var termineMisTareas = true;
+
+if(){
+
+}
